5624e432d148fee62d4c210f26061f5a2cc77352
Trixia Urquiza <trix.urquiza@gmail.com>
2015-02-18 10:06:25 +0800

    Added file

---
+++ practice/taurquiza/database/it111_excercise.sql
@@ -0,0 +1,58 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE SCHEMA IF NOT EXISTS `it111_excercise1` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `it111_excercise1` ;
+
+-- -----------------------------------------------------
+-- Table `it111_excercise1`.`region`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `it111_excercise1`.`region` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `region_code` VARCHAR(32) NULL,
+  `region_description` VARCHAR(32) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `it111_excercise1`.`province`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `it111_excercise1`.`province` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `province_code` VARCHAR(32) NULL,
+  `province_description` VARCHAR(32) NULL,
+  `region_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `region_id`),
+  INDEX `fk_province_region_idx` (`region_id` ASC),
+  CONSTRAINT `fk_province_region`
+    FOREIGN KEY (`region_id`)
+    REFERENCES `it111_excercise1`.`region` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `it111_excercise1`.`city`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `it111_excercise1`.`city` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `city_code` VARCHAR(32) NULL,
+  `city_description` VARCHAR(32) NULL,
+  `province_id` INT NOT NULL,
+  `province_region_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `province_id`, `province_region_id`),
+  INDEX `fk_city_province1_idx` (`province_id` ASC, `province_region_id` ASC),
+  CONSTRAINT `fk_city_province1`
+    FOREIGN KEY (`province_id` , `province_region_id`)
+    REFERENCES `it111_excercise1`.`province` (`id` , `region_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
