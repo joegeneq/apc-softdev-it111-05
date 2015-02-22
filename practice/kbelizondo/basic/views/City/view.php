42e96a0fa5e9e49cecdc2b00b3d92ca129eab606
Kimberly Elizondo <kmbelizondo@gmail.com>
2015-02-22 11:53:44 +0800

    Modified views.php inside /basic/views/City

--- practice/kbelizondo/basic/views/City/view.php
+++ practice/kbelizondo/basic/views/City/view.php
@@ -31,7 +31,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'city_code',
             'city_description',
-            'province_id',
+            'province.province_description',
         ],
     ]) ?>
 
