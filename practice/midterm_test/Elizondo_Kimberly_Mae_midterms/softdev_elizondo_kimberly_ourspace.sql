ecd7e394ea3e88a32c03c1e80e28b67689e2aa0b
Kimberly Elizondo <kmbelizondo@gmail.com>
2015-02-28 11:21:02 +0800

    Updated softdev_elizondo_kimberly_ourspace.sql

--- practice/midterm_test/Elizondo_Kimberly_Mae_midterms/softdev_elizondo_kimberly_ourspace.sql
+++ practice/midterm_test/Elizondo_Kimberly_Mae_midterms/softdev_elizondo_kimberly_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2015 at 04:05 AM
+-- Generation Time: Feb 28, 2015 at 04:19 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -37,7 +37,14 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   `landline` varchar(20) DEFAULT NULL,
   `cellphone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `myaddress`
+--
+
+INSERT INTO `myaddress` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `created_at`, `home_address`, `landline`, `cellphone`) VALUES
+(1, 'kimberly', 'belda', 'elizondo', 'F', '2015-02-28 03:18:45', '5096 Col. Santos St., South Cembo, Makati City', '1111111', '12345678901');
 
 -- --------------------------------------------------------
 
@@ -53,7 +60,14 @@ CREATE TABLE IF NOT EXISTS `mycomment` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `mycomment_ibfk_1` (`myaddress_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `mycomment`
+--
+
+INSERT INTO `mycomment` (`id`, `myaddress_id`, `author`, `body`, `created_at`) VALUES
+(1, 1, 'angelo sia', 'hi hello ', '2015-02-28 03:19:22');
 
 --
 -- Constraints for dumped tables
