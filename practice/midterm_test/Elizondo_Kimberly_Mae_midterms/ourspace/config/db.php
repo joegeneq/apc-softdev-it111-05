673d2a9df436b4e239809f7032322b20a7208ef2
Kimberly Elizondo <kmbelizondo@gmail.com>
2015-02-28 10:30:28 +0800

    Changed database connection to softdev_kimberly_elizondo_ourspace

--- practice/midterm_test/Elizondo_Kimberly_Mae_midterms/ourspace/config/db.php
+++ practice/midterm_test/Elizondo_Kimberly_Mae_midterms/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_kimberly_elizondo_ouspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
