e03b63c543eeb1073378dcda6b46f0072c647c19
Trixia Urquiza <trix.urquiza@gmail.com>
2015-02-28 11:59:55 +0800

    Added Values to mycomment table

--- practice/midterm_test/Urquiza_Trixia_midterms/softdev_urquiza_trixia_ourspace.sql
+++ practice/midterm_test/Urquiza_Trixia_midterms/softdev_urquiza_trixia_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2015 at 04:17 AM
+-- Generation Time: Feb 28, 2015 at 04:58 AM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.3
 
@@ -39,7 +39,14 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   `landline` varchar(20) DEFAULT NULL,
   `cellphone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `myaddress`
+--
+
+INSERT INTO `myaddress` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `created_at`, `home_address`, `landline`, `cellphone`) VALUES
+(1, 'Trixia Marie', 'Ambagan', 'Urquiza', 'F', '0000-00-00 00:00:00', '147 champaca st. western bicutan taguig city', '', '+639177760794');
 
 -- --------------------------------------------------------
 
@@ -55,7 +62,14 @@ CREATE TABLE IF NOT EXISTS `mycomment` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `mycomment_ibfk_1` (`myaddress_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `mycomment`
+--
+
+INSERT INTO `mycomment` (`id`, `myaddress_id`, `author`, `body`, `created_at`) VALUES
+(1, 1, 'Trixia Marie A. Urquiza', 'asdfghjgkhjdfgfgf', '2015-02-28 03:58:25');
 
 --
 -- Constraints for dumped tables
