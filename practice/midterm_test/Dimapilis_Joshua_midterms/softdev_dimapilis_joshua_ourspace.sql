ae97623c7d3f49f07d3ee63d6e162f4535920a72
Joshua Dimapilis <jdimapilis02@gmail.com>
2015-02-28 10:59:44 +0800

    Modified database by adding three additional fields, and re-exported sql.

--- practice/midterm_test/Dimapilis_Joshua_midterms/softdev_dimapilis_joshua_ourspace.sql
+++ practice/midterm_test/Dimapilis_Joshua_midterms/softdev_dimapilis_joshua_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2015 at 03:28 AM
+-- Generation Time: Feb 28, 2015 at 03:57 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -33,6 +33,9 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   `lastname` varchar(30) NOT NULL,
   `gender` varchar(1) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `home_address` varchar(50) DEFAULT NULL,
+  `landline` varchar(20) DEFAULT NULL,
+  `cellphone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
