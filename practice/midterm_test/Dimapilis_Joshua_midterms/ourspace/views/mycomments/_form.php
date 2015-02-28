75fbbef115b26b08b4e3c28632de1bf6be73c9ed
Joshua Dimapilis <jdimapilis02@gmail.com>
2015-02-28 11:16:48 +0800

    Changed Last Name to dropdown for My Comments Form

--- practice/midterm_test/Dimapilis_Joshua_midterms/ourspace/views/mycomments/_form.php
+++ practice/midterm_test/Dimapilis_Joshua_midterms/ourspace/views/mycomments/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\myaddress;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Mycomment */
@@ -13,6 +15,10 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'myaddress_id')->textInput() ?>
+    <?= $form->field($model, 'myaddress_id')->dropDownList(
+    		ArrayHelper::map(Myaddress::find()->all(), 'id', 'lastname'),
+        ['prompt'=>'Select Last Name']
+    ) ?>
 
     <?= $form->field($model, 'author')->textInput(['maxlength' => 255]) ?>
 
