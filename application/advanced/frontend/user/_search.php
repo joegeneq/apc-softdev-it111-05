59652c4c276a52f1a1dec382d6db9932827b6488
Trixia Urquiza <trix.urquiza@gmail.com>
2015-03-06 16:43:21 +0800

    Generated Models and Crud for user

---
+++ application/advanced/frontend/user/_search.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\UserSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'username') ?>
+
+    <?= $form->field($model, 'password') ?>
+
+    <?= $form->field($model, 'firstname') ?>
+
+    <?= $form->field($model, 'middlename') ?>
+
+    <?php // echo $form->field($model, 'lastname') ?>
+
+    <?php // echo $form->field($model, 'address') ?>
+
+    <?php // echo $form->field($model, 'contact_num') ?>
+
+    <?php // echo $form->field($model, 'user_type') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
