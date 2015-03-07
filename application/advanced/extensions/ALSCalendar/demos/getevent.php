69b500e03ea11ddb08a223ccbb57c1bd8f231210
Joshua Dimapilis <jdimapilis02@gmail.com>
2015-03-07 13:02:53 +0800

    Edited extensions, and db to ultimately revert to previous design, but with new database name

--- application/advanced/extensions/ALSCalendar/demos/getevent.php
+++ application/advanced/extensions/ALSCalendar/demos/getevent.php
@@ -2,15 +2,15 @@
 
 try {
 
-        $timestamp=strtotime("7 March 2016 ");
-        //echo gmdate("Y-m-d\TH:i:s\Z", $timestamp);
-        echo gmdate("Y-m-d", $timestamp);
+    $timestamp=strtotime("7 March 2016 ");
+    //echo gmdate("Y-m-d\TH:i:s\Z", $timestamp);
+    echo gmdate("Y-m-d", $timestamp);
 
     $x = date("W",strtotime("1 April 2015"));
     $y = date("W",strtotime("1 March 2015"));
     echo "<br>". $x - $y . "<br>";
 
-    $url = 'mysql:dbname=event;host=localhost';
+    $url = 'mysql:dbname=als;host=localhost';
     $username = 'root';
     $password = '';
 
@@ -18,7 +18,7 @@ try {
     $connection = new PDO($url, $username, $password);
 
     // Prepare and execute query
-    $query = "SELECT * FROM events";
+    $query = "SELECT id FROM date";
     $sth = $connection->prepare($query);
     $sth->execute();
 
@@ -29,8 +29,8 @@ try {
     while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
 
         $e = array();
-        $e['id'] = $row['event_id'];
-        $e['title'] = $row['event_name'];
+        $e['id'] = $row['id'];
+        $e['title'] = $row['sunday_reading'];
         $e['start'] = $row['start_event'];
 
         // Merge the event array into the return array
