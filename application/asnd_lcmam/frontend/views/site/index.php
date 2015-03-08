e400581fe84577fb597b5308649f87443a1fd7a2
Trixia Urquiza <trix.urquiza@gmail.com>
2015-03-08 23:55:33 +0800

    Added images to index.php

--- application/asnd_lcmam/frontend/views/site/index.php
+++ application/asnd_lcmam/frontend/views/site/index.php
@@ -5,12 +5,15 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Welcome <br>to<br> Ang Salita Ng Diyos!</h1>
-
+        <h1><img src="../web/images/Cardinal-tagle.jpg"></h1> 
+        <img src=" ../web/images/calendar-icon-md.png" width="150" height="175" title="Logo of a company" alt="Logo of a company" align="left">
+        <img src="../web/images/GDM-Icon-Admin-Client-Round.png" width="180" height="175" >
      	<!-- <p class="lead">You have successfully created your Yii-powered application.</p>
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>!-->
     </div>
 
+    
+<br>
     <div class="body-content">
 
         <div class="row">
