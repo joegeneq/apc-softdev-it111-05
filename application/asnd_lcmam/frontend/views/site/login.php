43583a45f360e984dbdf756e85f201b0608a2a8e
Trixia Urquiza <trix.urquiza@gmail.com>
2015-03-13 17:39:06 +0800

    Modified Login page

--- application/asnd_lcmam/frontend/views/site/login.php
+++ application/asnd_lcmam/frontend/views/site/login.php
@@ -7,26 +7,39 @@ use yii\bootstrap\ActiveForm;
 /* @var $model \common\models\LoginForm */
 
 $this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="site-login" align="center">
 
-    <p>Please fill out the following fields to login:</p>
 
-    <div class="row">
-        <div class="col-lg-5">
+   
+  <!--  <p>Please fill out the following fields to login:</p> !-->
+    <div class="row" position="relative">
+    
+        <div class="col-lg-5" position="absolute" align="center">
+        
+            <img src="../web/images/ALC_Logo.png" width="200" height="200" align="center">
+            <br>
+            
+            <br>
+
+        
+        
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
                 <?= $form->field($model, 'username') ?>
                 <?= $form->field($model, 'password')->passwordInput() ?>
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
                 <div style="color:#999;margin:1em 0">
                     If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
                 </div>
+
                 <div class="form-group">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
+
             <?php ActiveForm::end(); ?>
         </div>
+
     </div>
+    
 </div>
