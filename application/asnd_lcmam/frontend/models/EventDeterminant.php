0084a8c413291d3be7f8c47f80330b0deb8e26a1
Kimberly Elizondo <kmbelizondo@gmail.com>
2015-03-20 07:06:36 +0800

    Generated model EventDeterminant.php for table event_determinant

---
+++ application/asnd_lcmam/frontend/models/EventDeterminant.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "event_determinant".
+ *
+ * @property integer $id
+ * @property string $year
+ * @property string $sunday_cycle
+ * @property string $weekday_cycle
+ * @property string $week_ot_before_lent
+ * @property string $ash_wednesday
+ * @property string $easter_sunday
+ * @property string $pentecost_sunday
+ * @property string $week_ot_after_pentecost
+ * @property string $first_sunday_of_advent
+ */
+class EventDeterminant extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'event_determinant';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['year'], 'required'],
+            [['year', 'ash_wednesday', 'easter_sunday', 'pentecost_sunday', 'first_sunday_of_advent'], 'safe'],
+            [['sunday_cycle', 'weekday_cycle', 'week_ot_before_lent', 'week_ot_after_pentecost'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'year' => 'Year',
+            'sunday_cycle' => 'Sunday Cycle',
+            'weekday_cycle' => 'Weekday Cycle',
+            'week_ot_before_lent' => 'Week Ot Before Lent',
+            'ash_wednesday' => 'Ash Wednesday',
+            'easter_sunday' => 'Easter Sunday',
+            'pentecost_sunday' => 'Pentecost Sunday',
+            'week_ot_after_pentecost' => 'Week Ot After Pentecost',
+            'first_sunday_of_advent' => 'First Sunday Of Advent',
+        ];
+    }
+}
