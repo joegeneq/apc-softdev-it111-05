6bf30c64cb12f2748bb32b1ba9ce3dd48f720b0a
Kimberly Elizondo <kmbelizondo@gmail.com>
2015-03-20 06:45:44 +0800

    Updated database connection

--- application/asnd_lcmam/common/config/main-local.php
+++ application/asnd_lcmam/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=als',
+            'dsn' => 'mysql:host=localhost;dbname=asnd_lcmam',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
