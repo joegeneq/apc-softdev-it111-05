058df6bf8fcfab3fb9610b244b9a8d225fa8f9a5
Kimberly Elizondo <kmbelizondo@gmail.com>
2015-03-19 22:20:05 +0800

    Modified notes.php; inserted content

--- application/asnd_lcmam/frontend/views/site/notes.php
+++ application/asnd_lcmam/frontend/views/site/notes.php
@@ -10,8 +10,29 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+    
+    <h2>Useful Websites for Lectors and Commentators</h2>
 
+    <div id="container" style="text-indent:20px">
+    	<table border="1px" width="100%">
+    	<tr>
+    		<td><li><ul><h3>Web Link <a href="http://www.usccb.org/bible/" target="_blank">United States Conference of Catholic Bishops</a></h3></ul></li>
+    		<h4 style="text-indent: 100px">Daily Mass Readings based on the New American Bible Lectionary with podcast</p></h4>
+    	</tr>
+    	<tr>
+    		<td><li><ul><h3>Web Link <a href="http://netministries.org/Bbasics/bwords.htm" target="_blank">Biblical Words Pronunciation Guide</a></h3></ul></li>
+    		<h4 style="text-indent: 100px">Audio recordings of Biblical words' pronunciation.</p></h4>
+    	</tr>
+    	<tr>
+    		<td><li><ul><h3>Web Link <a href="http://www.wordandlife.org/downloads-2/" target="_blank">Downloads - Word & Life</a></h3></ul></li>
+    		<h4 style="text-indent: 100px">Download PDF copies of the Euchalette</p></h4>
+    	</tr>
+    	<tr>
+    		<td><li><ul><h3>Web Link <a href="http://ssp.ph/sambuhay/" target="_blank">Sambuhay</a></h3></ul></li>
+    		<h4 style="text-indent: 100px">To subscribe to Sambuhay Online</p></h4>
+    	</tr>
+    	</table>
+    </div>
     <!--<p>This is the About page. You may modify the following file to customize its content:</p>
 
     <code><?= __FILE__ ?></code>-->
