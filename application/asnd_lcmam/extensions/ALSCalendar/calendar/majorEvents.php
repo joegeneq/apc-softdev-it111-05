6874b34912a0613fc0af6726dd7ba067ab47b721
Joshua Dimapilis <jdimapilis02@gmail.com>
2015-03-24 23:09:53 +0800

    Updated majorEvents.php to view arranged readings for the 24th and the 25th of December

--- application/asnd_lcmam/extensions/ALSCalendar/calendar/majorEvents.php
+++ application/asnd_lcmam/extensions/ALSCalendar/calendar/majorEvents.php
@@ -58,14 +58,78 @@ try {
         while($row = $result->fetch_assoc()) {
             
             $e['title'] = $row['event_name'];
-            $e['start'] = $allSundays[$counter] . "T01:00:01";
-            $e['color'] = '#FFCC00';
+            $e['start'] = $year . $row['date'] . "T01:00:02";
+            $e['color'] = '#99FF66';
             $e['tip'] = $row['event_name'];
             $e['textColor'] = 'Black';
+            if ($row['event_type'] == "Christmas Dawn"){$e['start'] = $year . $row['date'] . "T01:00:02";}
+            if ($row['event_type'] == "Christmas Day"){$e['start'] = $year . $row['date'] . "T01:00:08";}
+            if ($row['event_type'] == "Christmas Night"){$e['start'] = $year . $row['date'] . "T01:00:14";}
+            if ($row['event_type'] == "Solemnity, Christmas - Nativity"){$e['start'] = $year . $row['date'] . "T01:00:02";}
+            if ($row['event_type'] == "Advent - Morning Mass"){$e['start'] = $year . $row['date'] . "T01:00:08";}
             if ($e['title'] != ""){ array_push($events, $e); }
 
-            //echo $e['start'];
-            array_push($events, $e);
+            $e['title'] = $row['event_first_reading'];
+            $e['start'] = $year . $row['date'] . "T01:00:03";
+            $e['color'] = '#99FF66';
+            $e['tip'] = $row['event_first_reading'];
+            $e['textColor'] = 'Black';
+            if ($row['event_type'] == "Christmas Dawn"){$e['start'] = $year . $row['date'] . "T01:00:03";}
+            if ($row['event_type'] == "Christmas Day"){$e['start'] = $year . $row['date'] . "T01:00:09";}
+            if ($row['event_type'] == "Christmas Day"){$e['start'] = $year . $row['date'] . "T01:00:08";}
+            if ($row['event_type'] == "Christmas Night"){$e['start'] = $year . $row['date'] . "T01:00:15";}
+            if ($row['event_type'] == "Solemnity, Christmas - Nativity"){$e['start'] = $year . $row['date'] . "T01:00:03";}
+            if ($row['event_type'] == "Advent - Morning Mass"){$e['start'] = $year . $row['date'] . "T01:00:09";}
+            if ($e['title'] != ""){ array_push($events, $e); }
+
+            $e['title'] = $row['event_second_reading'];
+            $e['start'] = $year . $row['date'] . "T01:00:04";
+            $e['color'] = '#99FF66';
+            $e['tip'] = $row['event_second_reading'];
+            $e['textColor'] = 'Black';
+            if ($row['event_type'] == "Christmas Dawn"){$e['start'] = $year . $row['date'] . "T01:00:04";}
+            if ($row['event_type'] == "Christmas Day"){$e['start'] = $year . $row['date'] . "T01:00:10";}
+            if ($row['event_type'] == "Christmas Night"){$e['start'] = $year . $row['date'] . "T01:00:16";}
+            if ($row['event_type'] == "Solemnity, Christmas - Nativity"){$e['start'] = $year . $row['date'] . "T01:00:04";}
+            if ($row['event_type'] == "Advent - Morning Mass"){$e['start'] = $year . $row['date'] . "T01:00:10";}
+            if ($e['title'] != ""){ array_push($events, $e); }
+
+            $e['title'] = $row['event_alleluia_verse'];
+            $e['start'] = $year . $row['date'] . "T01:00:05";
+            $e['color'] = '#99FF66';
+            $e['tip'] = $row['event_alleluia_verse'];
+            $e['textColor'] = 'Black';
+            if ($row['event_type'] == "Christmas Dawn"){$e['start'] = $year . $row['date'] . "T01:00:05";}
+            if ($row['event_type'] == "Christmas Day"){$e['start'] = $year . $row['date'] . "T01:00:11";}
+            if ($row['event_type'] == "Christmas Night"){$e['start'] = $year . $row['date'] . "T01:00:17";}
+            if ($row['event_type'] == "Solemnity, Christmas - Nativity"){$e['start'] = $year . $row['date'] . "T01:00:05";}
+            if ($row['event_type'] == "Advent - Morning Mass"){$e['start'] = $year . $row['date'] . "T01:00:11";}
+            if ($e['title'] != ""){ array_push($events, $e); }
+
+            $e['title'] = $row['event_responsorial_psalm'];
+            $e['start'] = $year . $row['date'] . "T01:00:06";
+            $e['color'] = '#99FF66';
+            $e['tip'] = $row['event_responsorial_psalm'];
+            $e['textColor'] = 'Black';
+            if ($row['event_type'] == "Christmas Dawn"){$e['start'] = $year . $row['date'] . "T01:00:06";}
+            if ($row['event_type'] == "Christmas Day"){$e['start'] = $year . $row['date'] . "T01:00:12";}
+            if ($row['event_type'] == "Christmas Night"){$e['start'] = $year . $row['date'] . "T01:00:18";}
+            if ($row['event_type'] == "Solemnity, Christmas - Nativity"){$e['start'] = $year . $row['date'] . "T01:00:06";}
+            if ($row['event_type'] == "Advent - Morning Mass"){$e['start'] = $year . $row['date'] . "T01:00:12";}
+            if ($e['title'] != ""){ array_push($events, $e); }
+
+            $e['title'] = $row['event_gospel'];
+            $e['start'] = $year . $row['date'] . "T01:00:07";
+            $e['color'] = '#99FF66';
+            $e['tip'] = $row['event_gospel'];
+            $e['textColor'] = 'Black';
+            if ($row['event_type'] == "Christmas Dawn"){$e['start'] = $year . $row['date'] . "T01:00:07";}
+            if ($row['event_type'] == "Christmas Day"){$e['start'] = $year . $row['date'] . "T01:00:13";}
+            if ($row['event_type'] == "Christmas Night"){$e['start'] = $year . $row['date'] . "T01:00:19";}
+            if ($row['event_type'] == "Solemnity, Christmas - Nativity"){$e['start'] = $year . $row['date'] . "T01:00:07";}
+            if ($row['event_type'] == "Advent - Morning Mass"){$e['start'] = $year . $row['date'] . "T01:00:13";}
+            if ($e['title'] != ""){ array_push($events, $e); }
+
         }
     } else {
         echo "Error on database connection. No results may be displayed.";
