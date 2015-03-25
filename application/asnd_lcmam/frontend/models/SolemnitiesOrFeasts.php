f1241ea898884e3395fe66a026ceb09aecd30398
Joshua Dimapilis <jdimapilis02@gmail.com>
2015-03-25 11:12:17 +0800

    Re-generated model for Solemnities or Feasts and Sunday Reading

--- application/asnd_lcmam/frontend/models/SolemnitiesOrFeasts.php
+++ application/asnd_lcmam/frontend/models/SolemnitiesOrFeasts.php
@@ -10,14 +10,6 @@ use Yii;
  * @property integer $id
  * @property string $date
  * @property string $title
- * @property string $first_reading
- * @property string $first_reading_audio
- * @property string $responsorial_psalm
- * @property string $responsorial_psalm_audio
- * @property string $second_reading
- * @property string $second_reading_audio
- * @property string $gospel
- * @property string $gospel_audio
  * @property string $type
  */
 class SolemnitiesOrFeasts extends \yii\db\ActiveRecord
@@ -36,9 +28,9 @@ class SolemnitiesOrFeasts extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['date', 'title'], 'required'],
+            [['date', 'title', 'type'], 'required'],
             [['date'], 'string', 'max' => 10],
-            [['title', 'first_reading', 'first_reading_audio', 'responsorial_psalm', 'responsorial_psalm_audio', 'second_reading', 'second_reading_audio', 'gospel', 'gospel_audio'], 'string', 'max' => 100],
+            [['title'], 'string', 'max' => 100],
             [['type'], 'string', 'max' => 1]
         ];
     }
@@ -52,14 +44,6 @@ class SolemnitiesOrFeasts extends \yii\db\ActiveRecord
             'id' => 'ID',
             'date' => 'Date',
             'title' => 'Title',
-            'first_reading' => 'First Reading',
-            'first_reading_audio' => 'First Reading Audio',
-            'responsorial_psalm' => 'Responsorial Psalm',
-            'responsorial_psalm_audio' => 'Responsorial Psalm Audio',
-            'second_reading' => 'Second Reading',
-            'second_reading_audio' => 'Second Reading Audio',
-            'gospel' => 'Gospel',
-            'gospel_audio' => 'Gospel Audio',
             'type' => 'Type',
         ];
     }
--- application/asnd_lcmam/frontend/models/SundayReading.php
+++ application/asnd_lcmam/frontend/models/SundayReading.php
@@ -9,6 +9,7 @@ use Yii;
  *
  * @property integer $id
  * @property integer $sunday_weeknum
+ * @property string $sunday_name
  * @property string $sunday_first_reading
  * @property string $sunday_first_audio
  * @property string $sunday_second_reading
@@ -23,6 +24,13 @@ use Yii;
  * @property string $sunday_before_gospel_audio
  * @property string $sunday_cycle_type
  * @property string $sunday_reading_type
+ * @property string $sunday_description
+ * @property string $sunday_first_optional
+ * @property string $sunday_second_optional
+ * @property string $sunday_responsorial_optional
+ * @property string $sunday_alleluia_optional
+ * @property string $sunday_gospel_optional
+ * @property string $sunday_before_gospel_optional
  */
 class SundayReading extends \yii\db\ActiveRecord
 {
@@ -42,8 +50,8 @@ class SundayReading extends \yii\db\ActiveRecord
         return [
             [['sunday_weeknum', 'sunday_cycle_type', 'sunday_reading_type'], 'required'],
             [['sunday_weeknum'], 'integer'],
+            [['sunday_name', 'sunday_before_gospel', 'sunday_before_gospel_audio', 'sunday_description', 'sunday_first_optional', 'sunday_second_optional', 'sunday_responsorial_optional', 'sunday_alleluia_optional', 'sunday_gospel_optional', 'sunday_before_gospel_optional'], 'string', 'max' => 100],
             [['sunday_first_reading', 'sunday_first_audio', 'sunday_second_reading', 'sunday_second_audio', 'sunday_alleluia_verse', 'sunday_alleluia_audio', 'sunday_responsorial_psalm', 'sunday_responsorial_audio', 'sunday_gospel', 'sunday_gospel_audio', 'sunday_reading_type'], 'string', 'max' => 45],
-            [['sunday_before_gospel', 'sunday_before_gospel_audio'], 'string', 'max' => 100],
             [['sunday_cycle_type'], 'string', 'max' => 1]
         ];
     }
@@ -56,6 +64,7 @@ class SundayReading extends \yii\db\ActiveRecord
         return [
             'id' => 'ID',
             'sunday_weeknum' => 'Sunday Weeknum',
+            'sunday_name' => 'Sunday Name',
             'sunday_first_reading' => 'Sunday First Reading',
             'sunday_first_audio' => 'Sunday First Audio',
             'sunday_second_reading' => 'Sunday Second Reading',
@@ -70,6 +79,13 @@ class SundayReading extends \yii\db\ActiveRecord
             'sunday_before_gospel_audio' => 'Sunday Before Gospel Audio',
             'sunday_cycle_type' => 'Sunday Cycle Type',
             'sunday_reading_type' => 'Sunday Reading Type',
+            'sunday_description' => 'Sunday Description',
+            'sunday_first_optional' => 'Sunday First Optional',
+            'sunday_second_optional' => 'Sunday Second Optional',
+            'sunday_responsorial_optional' => 'Sunday Responsorial Optional',
+            'sunday_alleluia_optional' => 'Sunday Alleluia Optional',
+            'sunday_gospel_optional' => 'Sunday Gospel Optional',
+            'sunday_before_gospel_optional' => 'Sunday Before Gospel Optional',
         ];
     }
 }
