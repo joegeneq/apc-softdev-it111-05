d27cac0b8def0b2e36c6ab4e300af532b40bb4cc
Kimberly Elizondo <kmbelizondo@gmail.com>
2015-03-27 20:03:44 +0800

    Updated files

---
+++ application/asnd_lcmam/frontend/views/site/activities.php
@@ -0,0 +1,22 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+$this->title = 'Calendar';
+//$this->params['breadcrumbs'][] = $this->title;
+
+//echo include("../../extensions/ALSCalendar/demos/calendar.php");
+
+?>
+
+<div class="site-calendar">
+ 
+     <h2>ALS CALENDAR</h2>
+  
+<object data='../../extensions/ALSCalendar/calendar/testCalendar.php' style='width:100%;min-height: 965px;'>
+
+    <!--<p>This is the About page. You may modify the following file to customize its content:</p>
+
+    <code><?= __FILE__ ?></code>-->
+</div>
+
