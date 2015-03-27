84714cce88433b0232a68f9840c42abd1a67448b
Trixia Marie A. Urquiza <trix.urquiza@gmail.com>
2015-03-27 14:08:22 +0800

    Added calendar.php for calendar view

---
+++ application/asnd_lcmam/frontend/views/site/calendar.php
@@ -0,0 +1,19 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View
+$this->params['breadcrumbs'][] = $this->title;*/
+$this->title = 'Calendar';
+?>
+
+
+
+<div class="site-about">
+  
+<object data='../../extensions/ALSCalendar/calendar/testCalendar.php' style='width:100%;min-height: 965px;'>
+
+
+    <!--<p>This is the About page. You may modify the following file to customize its content:</p>
+
+    <code><?= __FILE__ ?></code>-->
+</div>
