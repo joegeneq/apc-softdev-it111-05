1f46a7aad8b40877fafbdb70ce9f6f65dc42bfa4
Trixia Marie A. Urquiza <trix.urquiza@gmail.com>
2015-03-29 01:35:18 +0800

    Generated Models for SolemnitiesOrFeasts

--- application/asnd_lcmam/frontend/models/SolemnitiesOrFeasts.php
+++ application/asnd_lcmam/frontend/models/SolemnitiesOrFeasts.php
@@ -11,6 +11,18 @@ use Yii;
  * @property string $date
  * @property string $title
  * @property string $type
+ * @property string $first_reading
+ * @property string $first_reading_audio
+ * @property string $responsorial_psalm
+ * @property string $responsorial_psalm_audio
+ * @property string $second_reading
+ * @property string $second_reading_audio
+ * @property string $alleluia_verse
+ * @property string $alleluia_verse_audio
+ * @property string $gospel
+ * @property string $gospel_audio
+ * @property string $rule
+ * @property string $cycle_type
  */
 class SolemnitiesOrFeasts extends \yii\db\ActiveRecord
 {
@@ -31,7 +43,9 @@ class SolemnitiesOrFeasts extends \yii\db\ActiveRecord
             [['date', 'title', 'type'], 'required'],
             [['date'], 'string', 'max' => 10],
             [['title'], 'string', 'max' => 100],
-            [['type'], 'string', 'max' => 1]
+            [['type'], 'string', 'max' => 20],
+            [['first_reading', 'first_reading_audio', 'responsorial_psalm', 'responsorial_psalm_audio', 'second_reading', 'second_reading_audio', 'alleluia_verse', 'alleluia_verse_audio', 'gospel', 'gospel_audio'], 'string', 'max' => 60],
+            [['rule', 'cycle_type'], 'string', 'max' => 45]
         ];
     }
 
@@ -45,6 +59,18 @@ class SolemnitiesOrFeasts extends \yii\db\ActiveRecord
             'date' => 'Date',
             'title' => 'Title',
             'type' => 'Type',
+            'first_reading' => 'First Reading',
+            'first_reading_audio' => 'First Reading Audio',
+            'responsorial_psalm' => 'Responsorial Psalm',
+            'responsorial_psalm_audio' => 'Responsorial Psalm Audio',
+            'second_reading' => 'Second Reading',
+            'second_reading_audio' => 'Second Reading Audio',
+            'alleluia_verse' => 'Alleluia Verse',
+            'alleluia_verse_audio' => 'Alleluia Verse Audio',
+            'gospel' => 'Gospel',
+            'gospel_audio' => 'Gospel Audio',
+            'rule' => 'Rule',
+            'cycle_type' => 'Cycle Type',
         ];
     }
 }
--- application/asnd_lcmam/frontend/models/SolemnitiesOrFeastsSearch.php
+++ application/asnd_lcmam/frontend/models/SolemnitiesOrFeastsSearch.php
@@ -19,7 +19,7 @@ class SolemnitiesOrFeastsSearch extends SolemnitiesOrFeasts
     {
         return [
             [['id'], 'integer'],
-            [['date', 'title', 'type'], 'safe'],
+            [['date', 'title', 'type', 'first_reading', 'first_reading_audio', 'responsorial_psalm', 'responsorial_psalm_audio', 'second_reading', 'second_reading_audio', 'alleluia_verse', 'alleluia_verse_audio', 'gospel', 'gospel_audio', 'rule', 'cycle_type'], 'safe'],
         ];
     }
 
@@ -61,7 +61,19 @@ class SolemnitiesOrFeastsSearch extends SolemnitiesOrFeasts
 
         $query->andFilterWhere(['like', 'date', $this->date])
             ->andFilterWhere(['like', 'title', $this->title])
-            ->andFilterWhere(['like', 'type', $this->type]);
+            ->andFilterWhere(['like', 'type', $this->type])
+            ->andFilterWhere(['like', 'first_reading', $this->first_reading])
+            ->andFilterWhere(['like', 'first_reading_audio', $this->first_reading_audio])
+            ->andFilterWhere(['like', 'responsorial_psalm', $this->responsorial_psalm])
+            ->andFilterWhere(['like', 'responsorial_psalm_audio', $this->responsorial_psalm_audio])
+            ->andFilterWhere(['like', 'second_reading', $this->second_reading])
+            ->andFilterWhere(['like', 'second_reading_audio', $this->second_reading_audio])
+            ->andFilterWhere(['like', 'alleluia_verse', $this->alleluia_verse])
+            ->andFilterWhere(['like', 'alleluia_verse_audio', $this->alleluia_verse_audio])
+            ->andFilterWhere(['like', 'gospel', $this->gospel])
+            ->andFilterWhere(['like', 'gospel_audio', $this->gospel_audio])
+            ->andFilterWhere(['like', 'rule', $this->rule])
+            ->andFilterWhere(['like', 'cycle_type', $this->cycle_type]);
 
         return $dataProvider;
     }
