5f7b80c7ae01c93a9e125baf18fc23415fb0b840
Joshua Dimapilis <jdimapilis02@gmail.com>
2015-03-29 10:03:06 +0800

    Updated site controller to display about/home upon loading

--- application/asnd_lcmam/frontend/controllers/SiteController.php
+++ application/asnd_lcmam/frontend/controllers/SiteController.php
@@ -67,7 +67,7 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->render('about');
     }
 
     public function actionLogin()
