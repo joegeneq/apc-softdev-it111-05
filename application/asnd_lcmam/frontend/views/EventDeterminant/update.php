229b0a405d0060dec0b879add4c06e62779af35f
Kimberly Elizondo <kmbelizondo@gmail.com>
2015-03-30 08:15:03 +0800

    Changed 'id' to 'year' (event_determinant)

--- application/asnd_lcmam/frontend/views/EventDeterminant/update.php
+++ application/asnd_lcmam/frontend/views/EventDeterminant/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\EventDeterminant */
 
-$this->title = 'Update Event Determinant: ' . ' ' . $model->id;
+$this->title = 'Update Event Determinant: ' . ' ' . $model->year;
 $this->params['breadcrumbs'][] = ['label' => 'Event Determinants', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
