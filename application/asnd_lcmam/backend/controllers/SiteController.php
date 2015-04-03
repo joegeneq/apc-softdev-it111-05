71e94fab61594e3bf043157e9e7bdb89e3c21760
Trixia Marie A. Urquiza <trix.urquiza@gmail.com>
2015-04-03 11:15:11 +0800

    Modified SiteController to remove login

--- application/asnd_lcmam/backend/controllers/SiteController.php
+++ application/asnd_lcmam/backend/controllers/SiteController.php
@@ -22,11 +22,11 @@ class SiteController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['login', 'error'],
+                        'actions' => [],
                         'allow' => true,
                     ],
                     [
-                        'actions' => ['logout', 'index'],
+                        'actions' => [ 'index'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
