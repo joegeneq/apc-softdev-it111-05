ef43c4c8a11dfb73fabd435f6c2984f756514e09
Trixia Marie A. Urquiza <trix.urquiza@gmail.com>
2015-04-03 20:02:53 +0800

    Modified _form.php to remove duplicate attributes

--- application/asnd_lcmam/frontend/views/sunday-reading/_form.php
+++ application/asnd_lcmam/frontend/views/sunday-reading/_form.php
@@ -23,9 +23,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'sunday_first_optional')->textInput(['maxlength' => 100]) ?>
 
-
-    <?= $form->field($model, 'sunday_first_optional')->textInput(['maxlength' => 100]) ?>
-
     <?= $form->field($model, 'sunday_second_reading')->textInput(['maxlength' => 45]) ?>
 
     <?= $form->field($model, 'sunday_second_audio')->textInput(['maxlength' => 1000]) ?>
@@ -46,29 +43,6 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'sunday_responsorial_optional')->textInput(['maxlength' => 100]) ?>
 
 
-    <?= $form->field($model, 'sunday_responsorial_optional')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'sunday_gospel')->textInput(['maxlength' => 45]) ?>
-
-    <?= $form->field($model, 'sunday_gospel_audio')->textInput(['maxlength' => 1000]) ?>
-
-
-    <?= $form->field($model, 'sunday_before_gospel')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'sunday_before_gospel_audio')->textInput(['maxlength' => 1000]) ?>
-
-    <?= $form->field($model, 'sunday_before_gospel_optional')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'sunday_before_gospel_optional')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'sunday_gospel')->textInput(['maxlength' => 45]) ?>
-
-
-    <?= $form->field($model, 'sunday_cycle_type')->textInput(['maxlength' => 1]) ?>
-
-    <?= $form->field($model, 'sunday_reading_type')->textInput(['maxlength' => 45]) ?>
-
-
     <?= $form->field($model, 'sunday_gospel')->textInput(['maxlength' => 45]) ?>
 
     <?= $form->field($model, 'sunday_gospel_audio')->textInput(['maxlength' => 1000]) ?>
@@ -80,18 +54,6 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'sunday_reading_type')->textInput(['maxlength' => 45, 'disabled' => 'disabled']) ?>
 
     <?= $form->field($model, 'sunday_description')->textInput(['maxlength' => 100, 'disabled' => 'disabled']) ?>
-
-
-    <?= $form->field($model, 'sunday_gospel_audio')->textInput(['maxlength' => 1000]) ?>
-
-    <?= $form->field($model, 'sunday_gospel_optional')->textInput(['maxlength' => 100]) ?>
-    
-    <?= $form->field($model, 'sunday_cycle_type')->textInput(['maxlength' => 1, 'disabled' => 'disabled']) ?>
-
-    <?= $form->field($model, 'sunday_reading_type')->textInput(['maxlength' => 45, 'disabled' => 'disabled']) ?>
-
-    <?= $form->field($model, 'sunday_description')->textInput(['maxlength' => 100, 'disabled' => 'disabled']) ?>
-    
 
 
     <div class="form-group">
