00b7fa2e329f26f38ade1055131d0d8987e32c8a
Trixia Marie A. Urquiza <trix.urquiza@gmail.com>
2015-04-08 09:37:12 +0800

    Modified index.php

--- application/asnd_lcmam/backend/views/site/index.php
+++ application/asnd_lcmam/backend/views/site/index.php
@@ -5,12 +5,14 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 <br><br>
-    <!-- For Testing, using Local DB-->
-<object data='ALSCalendar/calendar/test/testCalendar.php' style='width:100%;min-height: 965px;'> 
-
 
 <!-- For Hosting, using DB of Host -->
 <object data='ALSCalendarForHost/calendar/test/testCalendar.php' style='width:100%;min-height: 965px;'>
+
+
+    <!-- For Testing, using Local DB-->
+<object data='ALSCalendar/calendar/test/testCalendar.php' style='width:100%;min-height: 965px;'> 
+
 
 
 </div>
